31ca7b6df938f834f568a5a7ade179d53f8f667a
xdx <xdx@xdxdeMBP.lan>
2019-02-24 21:43:52 +0800

    first commit

---
+++ LR/lr.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""lr model train"""
+
+
+from sklearn.linear_model import LogisticRegressionCV as LRCV
+
+
+def train(train_data_df):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,1 @@
+# PersonalRecommendationAlgorithm
